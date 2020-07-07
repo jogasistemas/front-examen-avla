9013122e87cd7eb85c0a9d1a06f263092c2fef75
jogasistemas <joga.sistemas@gmail.com>
2020-07-06 19:33:42 -0500

    mostrar  usuario en editar

--- src/app/admin/task-edit/task-edit.component.ts
+++ src/app/admin/task-edit/task-edit.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { TaskService } from '../services/task.service';
 
 import Task from '../../models/task.model';
+import { UserService } from '../services/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-task-edit',
@@ -12,17 +14,22 @@ import Task from '../../models/task.model';
 })
 export class TaskEditComponent implements OnInit {
   task: Task;
-
+  users: User[];
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private taskService: TaskService
+    private taskService: TaskService,
+    private userService: UserService
   ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.taskService.getTaskByid(+params.get('id'))
         .subscribe((task: Task) => this.task =  task);
+    });
+
+    this.userService.getAllUser().subscribe((Users: User[]) => {
+      this.users = Users;
     });
   }
 
